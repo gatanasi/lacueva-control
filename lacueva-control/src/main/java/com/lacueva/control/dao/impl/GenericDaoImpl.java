6a07e2d4920c93ec0199c1edb071041b3d993d05
Gabriel <gatanasios@gmail.com>
2015-06-30 13:08:06 -0300

    Minor HSQL config files

--- lacueva-control/src/main/java/com/lacueva/control/dao/impl/GenericDaoImpl.java
+++ lacueva-control/src/main/java/com/lacueva/control/dao/impl/GenericDaoImpl.java
@@ -21,7 +21,7 @@ public abstract class GenericDaoImpl<T> implements GenericDao<T> {
 	public GenericDaoImpl() {
 		Type t = getClass().getGenericSuperclass();
 		ParameterizedType pt = (ParameterizedType) t;
-		type = (Class) pt.getActualTypeArguments()[0];
+		type = (Class<T>) pt.getActualTypeArguments()[0];
 	}
 
 	@Override
